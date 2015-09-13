e7d0ff182e5f8a211a3718edf40bbf1bf06fb623
NickMilef <nbm5qe@virginia.edu>
2015-09-12 21:31:44 -0400

    Yay! GPS and input is working!

--- MusicLocation/app/src/main/java/edu/virginia/cs/musiclocation/MainActivity.java
+++ MusicLocation/app/src/main/java/edu/virginia/cs/musiclocation/MainActivity.java
@@ -1,16 +1,76 @@
 package edu.virginia.cs.musiclocation;
 
+import android.Manifest;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //GPS
+    private LocationListener location;
+    private LocationManager locManager;
+
+    private TextView test;
+    private TextView returnValue;
+    private EditText editValue;
+    private Button submitValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        submitValue=(Button) findViewById(R.id.returnInput);
+        returnValue=(TextView) findViewById(R.id.returned);
+        editValue=(EditText) findViewById(R.id.editText);
+
+        submitValue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                returnValue.setText(editValue.getText());
+            }
+        });
+
+        test=(TextView) findViewById(R.id.lat);
+        locManager=(LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        location=new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                test.setText(Double.toString(location.getLatitude()));
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        if (checkPermission(Manifest.permission.ACCESS_COARSE_LOCATION,0,0)>=0) {
+            locManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,100L,1.0f,location);
+        }
+
     }
 
     @Override

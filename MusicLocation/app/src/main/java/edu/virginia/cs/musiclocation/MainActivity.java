9c15268dfcd8cb99be66f4aeba460d6ac60fc64a
NickMilef <nbm5qe@virginia.edu>
2015-09-22 20:38:40 -0400

    changes

--- MusicLocation/app/src/main/java/edu/virginia/cs/musiclocation/MainActivity.java
+++ MusicLocation/app/src/main/java/edu/virginia/cs/musiclocation/MainActivity.java
@@ -3,6 +3,8 @@ package edu.virginia.cs.musiclocation;
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -17,6 +19,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.MediaController;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -37,20 +41,63 @@ public class MainActivity extends AppCompatActivity {
     private Button submitButton;
     private TextView locationLabel;
     private Button localButton;
+
     private String Stream_URL;
+    private String Image_URL;
+
+    private MediaPlayer mp;
+    private MediaController music_player;
+    private ImageView albumCover;
+    private Bitmap imageCoverArt;
+    private MediaController.MediaPlayerControl mp_control;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mp=new MediaPlayer();
 
         new downloadClass().execute();
 
+        albumCover = (ImageView) findViewById(R.id.album_cover);
         submitButton = (Button) findViewById(R.id.returnInput);
         returnValue = (TextView) findViewById(R.id.returned);
         editValue = (EditText) findViewById(R.id.editText);
         locationLabel = (TextView) findViewById(R.id.loc);
         localButton = (Button) findViewById(R.id.popularLink);
+        music_player = (MediaController) findViewById(R.id.music_controls);
+        music_player.setMediaPlayer(new MediaController.MediaPlayerControl() {
+            @Override
+            public boolean canSeekBackward() {
+                return false;
+            }
+
+            @Override
+            public boolean canPause() {
+                return true;
+            }
+
+            @Override
+            public int getBufferPercentage() {
+                return 0;
+            }
+
+            @Override
+            public boolean canSeekForward() {
+                return false;
+            }
+
+            @Override
+            public void start() {
+
+            }
+
+            @Override
+            public boolean isPlaying() {
+
+            }
+
+        });
 
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -97,7 +144,7 @@ public class MainActivity extends AppCompatActivity {
         };
 
         if (checkPermission(Manifest.permission.ACCESS_COARSE_LOCATION, 0, 0) >= 0) {
-            locManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+            locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
                     LOCATION_UPDATE_MIN_TIME, LOCATION_UPDATE_MIN_DIST, locationListener);
         }
     }
@@ -137,6 +184,9 @@ public class MainActivity extends AppCompatActivity {
                 BufferedReader stream3=new BufferedReader(stream2);
                 nextLine=stream3.readLine();
                 JSONString=JSONString+nextLine;
+                stream1.close();
+                stream2.close();
+                stream3.close();
             } catch (Exception e) {
                 Log.d("ERROR",e.toString());
                 Log.d("ERROR","Download Issue");
@@ -149,6 +199,20 @@ public class MainActivity extends AppCompatActivity {
                 object1=new JSONObject(JSONString);
                 Stream_URL=object1.getString("download_url");
                 Stream_URL=Stream_URL+"?client_id=a9e1ea232bfff486717273c718914e5b";
+
+                //Download image (could fail, but shouldn't stop the song from downloading)
+                try {
+                    Image_URL=object1.getString("artwork_url");
+                    Image_URL=Image_URL+"?client_id=a9e1ea232bfff486717273c718914e5b";
+                    InputStream image_stream=new URL(Image_URL).openStream();
+                    imageCoverArt=BitmapFactory.decodeStream(image_stream);
+                    image_stream.close();
+                } catch (Exception e) {
+                    Log.d("ERROR","Failed to download cover art");
+                    imageCoverArt=null;
+                }
+
+
             }
             catch (Exception e) {
                 Log.d("ERROR","JSON Issue");
@@ -159,10 +223,9 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onPostExecute(Integer input) {
-            MediaPlayer mp=new MediaPlayer();
             mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
             try {
-                Log.d("JSON",Stream_URL);
+                Log.d("JSON", Stream_URL);
                 mp.setDataSource(Stream_URL);
                 mp.prepare();
                 mp.start();
@@ -170,6 +233,14 @@ public class MainActivity extends AppCompatActivity {
             catch (Exception e) {
                 Log.d("ERROR",e.toString());
                 Log.d("ERROR","Stream Issue");
+            }
+
+            try {
+                if (imageCoverArt!=null) {
+                    albumCover.setImageBitmap(imageCoverArt);
+                }
+            } catch (Exception e) {
+                Log.d("ERROR","Album art couldn't be downloaded");
             }
         }
     }

a5374f75832aeb9f1287092718ce21321d49a947
Ra1nWarden <wzh19921016@gmail.com>
2015-09-30 21:00:29 -0400

    Start OnlineMusicPlayerActivity when the user selects a song from the popular list.

--- MusicLocation/app/src/main/java/edu/virginia/cs/musiclocation/PopularActivity.java
+++ MusicLocation/app/src/main/java/edu/virginia/cs/musiclocation/PopularActivity.java
@@ -1,11 +1,15 @@
 package edu.virginia.cs.musiclocation;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.widget.CursorAdapter;
 import android.support.v4.widget.SimpleCursorAdapter;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.TextView;
 
 public final class PopularActivity extends ListActivity {
 
@@ -22,6 +26,17 @@ public final class PopularActivity extends ListActivity {
             setListAdapter(new SimpleCursorAdapter(this, R.layout.song_list_entry, cursor, columns,
                     to, CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER));
         }
+
+        getListView().setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView label = (TextView) view.findViewById(R.id.song_id);
+                CharSequence songId = label.getText();
+                Intent i = new Intent(PopularActivity.this, OnlineMusicPlayerActivity.class);
+                i.putExtra(OnlineMusicPlayerActivity.TRACK_URL_KEY, songId);
+                startActivity(i);
+            }
+        });
     }
 
 }

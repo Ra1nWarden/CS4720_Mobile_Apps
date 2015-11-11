0a22090e2906f5a4d850a960a3afec78fdcaf5d8
Ra1nWarden <wzh19921016@gmail.com>
2015-11-11 01:12:18 -0500

    Set up tab view controllers.

---
+++ Project2/NickZihaoProject/NickZihaoProject/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  NickZihaoProject
+//
+//  Created by Zihao Wang on 11/11/15.
+//  Copyright © 2015年 Nick&Zihao. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.setHidesBackButton(true, animated: true)
+    }
+
+}
